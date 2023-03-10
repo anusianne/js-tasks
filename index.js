bbbae1edc740da6da9c475ebd8c0b581a708451c
anusianne <100295263+anusianne@users.noreply.github.com>
2023-03-10 20:37:52 +0100

    tree-list

--- index.js
+++ index.js
@@ -10,12 +10,12 @@
 //   ul.append(li);
 // }
 
-let ul = document.createElement("ul");
-	while(true){
-	newText = prompt("Input text for new list item");
-	if(newText == null){break;} 
-	let li = document.createElement("li");
-	li.innerHTML=newText;
-	ul.append(li);
-}
-document.body.append(ul);
+// let ul = document.createElement("ul");
+// 	while(true){
+// 	newText = prompt("Input text for new list item");
+// 	if(newText == null){break;} 
+// 	let li = document.createElement("li");
+// 	li.innerHTML=newText;
+// 	ul.append(li);
+// }
+// document.body.append(ul);
