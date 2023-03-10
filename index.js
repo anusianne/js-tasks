b43c5759f7ebe6cbfeb0dec293fa5bae731e239f
anusianne <100295263+anusianne@users.noreply.github.com>
2023-03-10 19:55:01 +0100

    add-create-list

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+// let ul = document.createElement('ul');
+// document.body.append(ul);
+// while (true) {
+//   let askUser = prompt('Enter a text to list elements', '');
+//   if (!askUser) {
+//     break;
+//   }
+//   let li = document.createElement('li');
+//   li.textContent = askUser;
+//   ul.append(li);
+// }
 
-let ul = document.createElement('ul');
+let ul = document.createElement("ul");
+	while(true){
+	newText = prompt("Input text for new list item");
+	if(newText == null){break;} 
+	let li = document.createElement("li");
+	li.innerHTML=newText;
+	ul.append(li);
+}
 document.body.append(ul);
-
-do {
-  let askUser = prompt('Enter a text to list elements', '');
-  let li = document.createElement('li');
-  document.body.append(li);
-} while (askUser);
-
