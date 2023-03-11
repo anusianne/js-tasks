f4b7e01ed9b253e4766e101db975dbe34804b7bf
anusianne <100295263+anusianne@users.noreply.github.com>
2023-03-11 21:50:05 +0100

    string methods primary added

---
+++ string_methods.js
@@ -0,0 +1,10 @@
+let msg = 'Leave me alone';
+console.log(msg.toUpperCase());
+console.log(msg.toLowerCase());
+let msg_space = '    Leave me alone   ';
+console.log(msg_space.trim());
+console.log("haha weel soon".indexOf("s"));
+let str = 'Welcome and happy new year';
+str.slice(2,5);
+str.replace('Welcome', 'Merry Christmas');
+'Happy birthday to you '.repeat(2);
